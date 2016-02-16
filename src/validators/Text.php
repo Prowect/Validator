1604a0cabdf5d6f055f5c4bde672914ad2695e9c
codenamesteel <Alexander@Air0.local>
2016-02-16 19:43:49 +0100

    Added Text + IP

---
+++ src/validators/Text.php
@@ -0,0 +1,20 @@
+<?PHP
+
+namespace Drips\Validator\validators;
+
+use Drips\Validator\IValidator;
+
+class Text implements IValidator {
+
+public static function validate($input){
+
+return ctype_alpha($input);
+
+}
+
+
+
+}
+
+
+?>
