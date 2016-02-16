f2a2b105e5fb85f52216967e18674a17d587cd32
codenamesteel <Alexander@Air0.local>
2016-02-16 19:34:31 +0100

    Add Number

---
+++ src/validators/Number.php
@@ -0,0 +1,20 @@
+<?PHP
+
+namespace Drips\Validator\validators;
+
+use Drips\Validator\IValidator;
+
+class Number implements IValidator {
+
+public static function validate($input){
+
+return is_numeric($input);
+
+}
+
+
+
+}
+
+
+?>
