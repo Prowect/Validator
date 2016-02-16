f63d9f1f0b13a4bba4d69522a3de05f080225144
codenamesteel <Alexander@Air0.local>
2016-02-16 19:50:07 +0100

    Added URL

---
+++ src/validators/Url.php
@@ -0,0 +1,19 @@
+<?PHP
+
+namespace Drips\Validator\validators;
+
+use Drips\Validator\IValidator;
+
+class Url implements IValidator {
+
+public static function validate($input){
+
+return filter_var($input, FILTER_VALIDATE_URL);
+
+}
+
+
+
+}
+
+?>
