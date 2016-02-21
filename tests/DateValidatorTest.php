65e1e8d82579f000e57a48418c733349cb2fabaf
codenamesteel <Alexander@192.168.0.8>
2016-02-21 14:31:20 +0100

    Added DateValidatorTest.php

---
+++ tests/DateValidatorTest.php
@@ -0,0 +1,27 @@
+<?PHP
+
+namespace tests;
+
+use Drips\Validator\validators\Email;
+use PHPUnit_Framework_TestCase;
+
+include(__DIR__."/../vendor/autoload.php");
+
+class DateValidatorTest extends PHPUnit_Framework_TestCase {
+
+/**
+ * @dataProvider dateProvider
+*/
+public function testDate($date, $result){
+	$this->assertEquals(Email::validate($date),$result);
+}
+
+public function dateProvider(){
+	return array(array("12.06.2006", true), array("32.07.2015", false), array("1.1.16", true), array("29.02.1995", true));
+}
+
+
+
+}
+
+?>
