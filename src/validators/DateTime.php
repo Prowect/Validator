89ecbd60574b9a9b59c33f565e03de63710a8916
codenamesteel <Alexander@192.168.0.8>
2016-02-21 01:31:52 +0100

    Update

---
+++ src/validators/DateTime.php
@@ -0,0 +1,13 @@
+<?PHP
+
+namespace Drips\Validator\validators;
+
+use Drips\Validator\IValidator;
+
+class DateTime implements IValidator
+{
+    public static function validate($input)
+    {
+      return Date::validate($input) != false && Time::validate($input) != false;
+    }
+}
