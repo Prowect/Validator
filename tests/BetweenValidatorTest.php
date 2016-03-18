aba8745d1f639c092cdbf7f6b6839cc1a6639cb7
sorrina <1981maariaa@gmail.com>
2016-03-18 22:03:48 +0100

    syntaxerror gefixt

--- tests/BetweenValidatorTest.php
+++ tests/BetweenValidatorTest.php
@@ -22,7 +22,7 @@ class BetweenValidatorTest extends PHPUnit_Framework_TestCase
         return array(
 			array(1, 5, 0, false),
             array("1", 0, 5, true),
-            array("abcde", 3, 4 false),
+            array("abcde", 3, 4, false),
 			array(9, 5, 11, true),
 			array(6, 0, 4, false),
 			array("3", 1, 9, true)
