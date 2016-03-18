9f9894c7ba73c534af610862ee0619aa4ca1c2d4
sorrina <1981maariaa@gmail.com>
2016-03-18 21:04:26 +0100

    MinlengthTestValidator gefixt

--- src/validators/Minlength.php
+++ src/validators/Minlength.php
@@ -5,7 +5,7 @@ use Drips\Validator\IValidator;
 
 class Minlength implements IValidator
 {
-    public static function validate($input, $length)
+    public static function validate($input, $length = 3)
     {
 		return strlen($input) >= $length;
     }
